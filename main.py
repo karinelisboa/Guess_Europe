8eb9369fdc220c0e8514af4fd2a02ab5e5847a12
karinelisboa <karinelisboarodrigues@gmail.com>
2024-03-26 08:02:20 -0300

    Mechanics added

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from turtle import Turtle, Screen
+import pandas as pd
 
 image = 'map_europe.gif'
 
 turtle = Turtle()
 screen = Screen()
 game_on = True
+list_countries = []
+guessed_countries = []
+
+data = pd.read_csv('some_countries.csv')
+
+for country in data.states:
+    list_countries.append(country)
 
 while game_on:
     screen.addshape(image)
@@ -14,5 +22,15 @@ while game_on:
 
     def get_mouse_click_coor(x, y):
         print(x, y)
+
+    answer = screen.textinput(f"{len(guessed_countries)}/{len(list_countries)}", "Guess a european country:")
+    if answer.title() in list_countries:
+        guessed_countries.append(answer)
+        country = Turtle()
+        country.hideturtle()
+        country.penup()
+        country_data = data[data.states == answer.title()]
+        country.goto(int(country_data['x']), int(country_data['y']))
+        country.write(f"{answer.title()}")
 
     screen.onclick(get_mouse_click_coor)
